e7ac9e852872599f67952b545e7727fc8b0705c3
yosra-wan <yosrawanene28@gmail.com>
2023-11-24 21:46:39 +0100

    dockerfile + jenkinsfile

--- auth-service/src/main/java/com/example/authentication/config/SecurityConfig.java
+++ auth-service/src/main/java/com/example/authentication/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.authentication.config;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
